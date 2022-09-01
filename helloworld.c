91ad37820acd5a5de4dc2ae74f474c36dabd7aa3
keshunhu <1194156321@qq.com>
2022-09-01 11:16:11 +0800

    helloworld

---
+++ helloworld.c
@@ -0,0 +1,15 @@
+/*************************************************************************
+    > File Name: helloworld.c
+    > Author: zll
+    > Mail: zhnllion@126.com 
+    > Created Time: 四  9/ 1 11:14:56 2022
+ ************************************************************************/
+
+#include<stdio.h>
+int main()
+{
+
+	printf("hello world \n");
+
+	return 0;
+}
